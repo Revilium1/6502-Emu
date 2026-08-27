692c0512170036cd757bf947c306753791f957e3
Revilium1 <randomdance52@outlook.com>
2026-08-26 22:46:44 -0800

    implement LDA_ABS

--- 6502/6502Lib/src/public/main_6502.h
+++ 6502/6502Lib/src/public/main_6502.h
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// http://www.obelisk.me.uk/6502/
+// https://6502.org/users/obelisk/
 
 using Byte = unsigned char;
 using Word = unsigned short;
@@ -9,34 +9,29 @@ using Word = unsigned short;
 using u32 = unsigned int;
 using s32 = signed int;
 
-struct Mem
-{
-	static constexpr u32 MAX_MEM = 1024 * 64;
-	Byte Data[MAX_MEM];
+struct Mem {
+    static constexpr u32 MAX_MEM = 1024 * 64;
+    Byte Data[MAX_MEM];
 
-	void Initialise()
-	{
-		for ( u32 i = 0; i < MAX_MEM; i++ )
-		{
-			Data[i] = 0;
-		}
-	}
+    void Initialise() {
+        for (u32 i = 0; i < MAX_MEM; i++) {
+            Data[i] = 0;
+        }
+    }
 
-	/** read 1 byte */
-	Byte operator[]( u32 Address ) const
-	{
-		// assert here Address is < MAX_MEM
-		return Data[Address];
-	}
+    /** read 1 byte */
+    Byte operator[](u32 Address) const {
+        // assert here Address is < MAX_MEM
+        return Data[Address];
+    }
 
-	/** write 1 byte */
-	Byte& operator[]( u32 Address )
-	{
-		// assert here Address is < MAX_MEM
-		return Data[Address];
-	}
+    /** write 1 byte */
+    Byte & operator[](u32 Address) {
+        // assert here Address is < MAX_MEM
+        return Data[Address];
+    }
 
-    void WriteWord(Word Value, u32 Address, s32& Cycles) {
+    void WriteWord(Word Value, u32 Address, s32 & Cycles) {
         Data[Address] = Value & 0xFF;
         Data[Address + 1] = (Value >> 8);
         Cycles -= 2;
@@ -44,21 +39,20 @@ struct Mem
 };
 
 struct CPU {
-    Word PC;        // program counter
-    Word SP;        // stack pointer
+    Word PC; // program counter
+    Word SP; // stack pointer
 
-    Byte A, X, Y;   // registers
+    Byte A, X, Y; // registers
 
-    Byte C : 1;     // status flags
-    Byte Z : 1;
-    Byte I : 1;
-    Byte D : 1;
-    Byte B : 1;
-    Byte V : 1;
-    Byte N : 1;
+    Byte C: 1; // status flags
+    Byte Z: 1;
+    Byte I: 1;
+    Byte D: 1;
+    Byte B: 1;
+    Byte V: 1;
+    Byte N: 1;
 
-    void Reset(Mem& memory) 
-    {
+    void Reset(Mem & memory) {
         PC = 0xFFFC;
         SP = 0x0100;
         C = Z = I = D = B = Y = N = 0;
@@ -66,20 +60,20 @@ struct CPU {
         memory.Initialise();
     }
 
-    Byte FetchByte(s32& Cycles, Mem& memory) {
+    Byte FetchByte(s32 & Cycles, Mem & memory) {
         Byte Data = memory[PC];
         PC++;
         Cycles--;
         return Data;
     }
 
-    Byte ReadByte(s32& Cycles, Byte Address, Mem& memory) {
+	Byte ReadByte(s32 & Cycles, Word Address, Mem & memory) {
         Byte Data = memory[Address];
         Cycles--;
         return Data;
     }
 
-    Word FetchWord(s32& Cycles, Mem& memory) {
+    Word FetchWord(s32 & Cycles, Mem & memory) {
         // 6502 is little endian
         Word Data = memory[PC];
         PC++;
@@ -93,69 +87,69 @@ struct CPU {
     }
 
     // opcodes
-	static constexpr Byte
-		INS_LDA_IM = 0xA9,
-		INS_LDA_ZP = 0xA5,
-		INS_LDA_ZPX = 0xB5,
+    static constexpr Byte
+    INS_LDA_IM = 0xA9,
+        INS_LDA_ZP = 0xA5,
+        INS_LDA_ZPX = 0xB5,
         INS_LDA_ABS = 0xAD,
         INS_LDA_ABSX = 0xBD,
         INS_LDA_ABSY = 0xB9,
         INS_LDA_INDX = 0xA1,
         INS_LDA_INDY = 0xB1,
-		INS_JSR = 0x20;
+        INS_JSR = 0x20;
 
     void LDASetStatus() {
         Z = (A == 0);
         N = (A & 0b10000000) > 0;
     }
 
-    s32 Execute(s32 Cycles, Mem& memory) 
-    {
+    s32 Execute(s32 Cycles, Mem & memory) {
         const s32 CyclesRequested = Cycles;
-        while (Cycles > 0)
-        {
+        while (Cycles > 0) {
             Byte Ins = FetchByte(Cycles, memory);
             switch (Ins) {
-                case INS_LDA_IM:
-                {
-                    Byte Value = 
-                        FetchByte(Cycles, memory);
-                    A = Value;
-                    LDASetStatus();
-                } break;
+            case INS_LDA_IM: {
+                Byte Value =
+                    FetchByte(Cycles, memory);
+                A = Value;
+                LDASetStatus();
+            } break;
 
-                case INS_LDA_ZP:
-                {
-                    Byte ZeroPageAddress = FetchByte(Cycles,memory);
-                    A = ReadByte(Cycles, ZeroPageAddress, memory);
-                    LDASetStatus();
-                } break;
+            case INS_LDA_ZP: {
+                Byte ZeroPageAddress = FetchByte(Cycles, memory);
+                A = ReadByte(Cycles, ZeroPageAddress, memory);
+                LDASetStatus();
+            } break;
 
-                			case INS_LDA_ZPX:
-			{
-				Byte ZeroPageAddr =
-					FetchByte( Cycles, memory );
-				ZeroPageAddr += X;
-				Cycles--;
-				A = ReadByte(
-					Cycles, ZeroPageAddr, memory );
-				LDASetStatus();
-			} break;
-			case INS_JSR:
-			{
-				Word SubAddr = 
-					FetchWord( Cycles, memory );
-				memory.WriteWord( 
-					PC - 1, SP, Cycles );
-				SP += 2;
-				PC = SubAddr;
-				Cycles--;
+            case INS_LDA_ZPX: {
+                Byte ZeroPageAddr =
+                    FetchByte(Cycles, memory);
+                ZeroPageAddr += X;
+                Cycles--;
+                A = ReadByte(
+                    Cycles, ZeroPageAddr, memory);
+                LDASetStatus();
+            } break;
+
+			case INS_LDA_ABS: {
+				Word AbsAddress = FetchWord(Cycles, memory);
+				A = ReadByte(Cycles, AbsAddress, memory);
+
 			} break;
 
-                default:
-                {
-                    printf("Instruction %d not implemented\n", Ins);
-                } break;
+            case INS_JSR: {
+                Word SubAddr =
+                    FetchWord(Cycles, memory);
+                memory.WriteWord(
+                    PC - 1, SP, Cycles);
+                SP += 2;
+                PC = SubAddr;
+                Cycles--;
+            } break;
+
+            default: {
+                printf("Instruction %d not implemented\n", Ins);
+            } break;
             }
         }
         const s32 CyclesUsed = CyclesRequested - Cycles;
